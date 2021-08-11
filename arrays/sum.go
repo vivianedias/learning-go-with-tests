565e754ba8d542a6d6c09815369c52aff35ecdb5
Viviane Dias <vivipirolad@gmail.com>
2021-08-11 16:43:49 -0300

    Adds func SumAll

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,18 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	// Stores the length of the array numbersToSum
+	lengthOfNumbers := len(numbersToSum)
+	// Creates an array with the size of lengthOfNumbers
+	sums := make([]int, lengthOfNumbers)
+
+	// Stores the array inside numbers
+	// Allocates the result of Sum(numbers) into the respective sums index
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
